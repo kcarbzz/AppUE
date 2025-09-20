95087601b8e38bac74f028429ea3bd8736a17248
Ruben <>
2025-09-20 11:18:48 -0500

    llll

--- app/src/main/java/com/example/appue/presentation/home/Welcome.kt
+++ app/src/main/java/com/example/appue/presentation/home/Welcome.kt
@@ -1,2 +1,35 @@
 package com.example.appue.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun Welcome(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "¡Bienvenido!",
+            style = MaterialTheme.typography.displayLarge
+        )
+    }
+}
--- app/src/main/java/com/example/appue/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/appue/presentation/navigation/AppNavGraph.kt
@@ -1,2 +1,26 @@
 package com.example.appue.presentation.navigation
 
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appue.presentation.auth.LoginScreen
+import com.example.appue.presentation.auth.RegisterScreen
+import com.example.appue.presentation.home.Welcome
+
+@Composable
+fun AppNavGraph(){
+    val navController = rememberNavController()
+
+
+    NavHost(navController = navController,
+        startDestination = "register"){
+        composable("register"){ RegisterScreen(navController) }
+        composable("login"){ LoginScreen(navController) }
+        composable("Welcome"){ Welcome(navController) }
+
+
+    }
+
+}
